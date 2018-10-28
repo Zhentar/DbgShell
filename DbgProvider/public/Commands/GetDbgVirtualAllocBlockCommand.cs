d22a259681625f993eefdfb0ccfe80555efb229d
Zhentar <zhentar@gmail.com>
2018-10-28 18:49:12 -0500

    Add initial virtualalloc block command

---
+++ DbgProvider/public/Commands/GetDbgVirtualAllocBlockCommand.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Management.Automation;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Diagnostics.Runtime.Interop;
+
+namespace MS.Dbg.Commands
+{
+	[Cmdlet(VerbsCommon.Get, "DbgVirtualAllocBlock")]
+	[OutputType(typeof(DbgVirtualAllocBlock))]
+	public class GetDbgVirtualAllocBlockCommand : DbgBaseCommand
+	{
+		[Parameter(Mandatory = false,
+			Position = 0,
+			ValueFromPipeline = true,
+			ValueFromPipelineByPropertyName = true)]
+		[AddressTransformation]
+		public ulong Address { get; set; }
+
+		protected override void ProcessRecord()
+		{
+			base.ProcessRecord();
+
+			if (Address != 0)
+			{
+				WriteObject(new DbgVirtualAllocBlock(Address, Debugger));
+			}
+			else
+			{
+				foreach (var block in DbgVirtualAllocBlock.AllBlocks(Debugger))
+				{
+					WriteObject(block);
+				}
+			}
+		}
+	}
+}
---
+++ DbgProvider/public/Debugger/DbgVirtualAllocBlock.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Diagnostics.Runtime.Interop;
+
+namespace MS.Dbg
+{
+	public class DbgVirtualAllocBlock
+	{
+		public static IEnumerable<DbgVirtualAllocBlock> AllBlocks(DbgEngDebugger debugger)
+		{
+			ulong address = 0;
+			while (debugger.TryQueryVirtual(address, out var info) == 0)
+			{
+				if (info.State.HasFlag(MEM.FREE))
+				{
+					address += info.RegionSize;
+				}
+				else
+				{
+					var block = new DbgVirtualAllocBlock(address, debugger);
+					address = block.BaseAddress + block.BlockSize;
+					yield return block;
+				}
+			}
+		}
+
+		public ulong BaseAddress { get; }
+
+		public ulong BlockSize { get;  }
+		
+		public ulong CommitSize { get; }
+
+		public MEM Type { get; }
+
+		public DbgVirtualAllocBlock(ulong addr, DbgEngDebugger debugger)
+		{
+			var info = debugger.QueryVirtual(addr);
+			Type = info.Type; //I am under the impression that an alloc block can only be one type
+			var commitSize = 0ul;
+			ulong currAddress = BaseAddress = info.AllocationBase;
+			while (debugger.TryQueryVirtual(currAddress, out info) == 0 && info.AllocationBase == BaseAddress)
+			{
+				currAddress += info.RegionSize;
+				if (info.State.HasFlag(MEM.COMMIT))
+				{
+					commitSize += info.RegionSize;
+				}
+			}
+
+			BlockSize = currAddress - BaseAddress;
+			CommitSize = commitSize;
+			Debugger = debugger;
+		}
+
+		private readonly DbgEngDebugger Debugger;
+
+		public string Description
+		{
+			get
+			{
+				switch (Type)
+				{
+					case MEM.IMAGE:
+						try
+						{
+							return Debugger.GetModuleByAddress(BaseAddress).Name;
+						}
+						catch (DbgEngException)
+						{
+							return "<unknown>";
+						}
+					case MEM.MAPPED:
+					case MEM.PRIVATE:
+					default:
+						return "";
+				}
+			}
+		}
+	}
+}
