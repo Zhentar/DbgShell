be19ae80141ca606ec36f8d76a1068c364b4596b
Zhentar <zhentar@gmail.com>
2018-10-28 18:53:14 -0500

    Fix indentation format, add editorconfig for it

--- DbgProvider/public/Commands/GetDbgVirtualAllocBlockCommand.cs
+++ DbgProvider/public/Commands/GetDbgVirtualAllocBlockCommand.cs
@@ -1,39 +1,33 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Management.Automation;
-using System.Text;
-using System.Threading.Tasks;
-using Microsoft.Diagnostics.Runtime.Interop;
+﻿using System.Management.Automation;
 
 namespace MS.Dbg.Commands
 {
-	[Cmdlet(VerbsCommon.Get, "DbgVirtualAllocBlock")]
-	[OutputType(typeof(DbgVirtualAllocBlock))]
-	public class GetDbgVirtualAllocBlockCommand : DbgBaseCommand
-	{
-		[Parameter(Mandatory = false,
-			Position = 0,
-			ValueFromPipeline = true,
-			ValueFromPipelineByPropertyName = true)]
-		[AddressTransformation]
-		public ulong Address { get; set; }
+    [Cmdlet(VerbsCommon.Get, "DbgVirtualAllocBlock")]
+    [OutputType(typeof(DbgVirtualAllocBlock))]
+    public class GetDbgVirtualAllocBlockCommand : DbgBaseCommand
+    {
+        [Parameter(Mandatory = false,
+            Position = 0,
+            ValueFromPipeline = true,
+            ValueFromPipelineByPropertyName = true)]
+        [AddressTransformation]
+        public ulong Address { get; set; }
 
-		protected override void ProcessRecord()
-		{
-			base.ProcessRecord();
+        protected override void ProcessRecord()
+        {
+            base.ProcessRecord();
 
-			if (Address != 0)
-			{
-				WriteObject(new DbgVirtualAllocBlock(Address, Debugger));
-			}
-			else
-			{
-				foreach (var block in DbgVirtualAllocBlock.AllBlocks(Debugger))
-				{
-					WriteObject(block);
-				}
-			}
-		}
-	}
+            if (Address != 0)
+            {
+                WriteObject(new DbgVirtualAllocBlock(Address, Debugger));
+            }
+            else
+            {
+                foreach (var block in DbgVirtualAllocBlock.AllBlocks(Debugger))
+                {
+                    WriteObject(block);
+                }
+            }
+        }
+    }
 }
